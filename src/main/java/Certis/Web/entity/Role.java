bb24a38b173548e5e0830bbadf687178571e85e4
Coffee-Poter <113098614+Coffee-Poter@users.noreply.github.com>
2023-07-27 22:10:09 +0900

    OAuth2 Login & Assign

--- src/main/java/Certis/Web/entity/Role.java
+++ src/main/java/Certis/Web/entity/Role.java
@@ -1,16 +1,6 @@
 package Certis.Web.entity;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
 public enum Role {
-
-    GUEST("ROLE_GUEST", "손님"),
-    USER("ROLE_USER", "사용자"),
-    ADMIN("ROLE_ADMIN", "관리자");
-
-    private final String key;
-    private final String title;
+    ROLE_USER, ROLE_MANAGER, ROLE_ADMIN
 }
+
